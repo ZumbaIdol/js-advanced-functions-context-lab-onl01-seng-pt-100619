46174ff97a3c5c7618d0b34c8bae67f8fa1684cc
ZumbaIdol <david.a.foote@gmail.com>
2020-05-26 16:07:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 /* Your Code Here */
+let createEmployeeRecord = function(ele) {
+    return {
+        firstName: ele[0],
+        familyName: ele[1],
+        title: ele[2],
+        payPerHour: ele[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(employeeEleInfo) {
+    return employeeEleInfo.map(function(ele) {
+        return createEmployeeRecord(ele)
+    })
+}
+
+let createTimeInEvent = function(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ')
+    employee.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
+let createTimeOutEvent = function(employee, dateTime) {
+    let [date, hour] = dateTime.split(' ')
+    employee.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return employee
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
